5976b6f569b43906740e2222303e063524970c2b
Oscar Mier <voscarmv@gmail.com>
2020-06-10 14:09:02 -0500

    Corrected Options migration.

--- db/migrate/20200611154224_create_opinions.rb
+++ db/migrate/20200611154224_create_opinions.rb
@@ -2,7 +2,7 @@ class CreateOpinions < ActiveRecord::Migration[6.0]
   def change
     create_table :opinions do |t|
       t.text :content
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_162650) do
+ActiveRecord::Schema.define(version: 2020_06_11_154224) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 2020_06_10_162650) do
 
   create_table "opinions", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id_id"], name: "index_opinions_on_user_id_id"
+    t.index ["user_id"], name: "index_opinions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,5 +54,5 @@ ActiveRecord::Schema.define(version: 2020_06_10_162650) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "opinions", "user_ids"
+  add_foreign_key "opinions", "users"
 end
