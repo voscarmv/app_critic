c4e118da454fd7a42b3326f936719bae683f53fd
Oscar Mier <voscarmv@gmail.com>
2020-06-12 14:27:14 -0500

    Basic user show page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   resources :followings
   devise_for :users
+  resources :users
   root "opinions#new"
   resources :opinions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
